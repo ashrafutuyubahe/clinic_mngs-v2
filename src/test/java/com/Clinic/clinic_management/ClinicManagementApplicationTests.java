c0553fec89a1df7328906cdd03df816a2cf1f81a
ashrafutuyubahe <ashraftuyubahe001@gmail.com>
2025-05-06 09:46:26 +0200

    first commit

---
+++ src/test/java/com/Clinic/clinic_management/ClinicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Clinic.clinic_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
